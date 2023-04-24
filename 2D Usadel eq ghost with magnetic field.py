Fixed sign error and calculation of B-field
Fixed two sign errors in the linearized Usadel equation. Also fixed the calculation of the B-field for the superconductor surrounding the normal metal when N is odd, though if N=4n+1 this still does not work. Finally fixed an error where division by the inverse distances was done instead of multiplication when calculating the induced A-field.

2D Usadel eq ghost with magnetic field.py
@@ -4,6 +4,8 @@ Created on Mon Jan 30 13:55:36 2023
 
 @author: Havard
 """
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate as spint
@@ -22,46 +24,15 @@ t1 = time.time()
 
 
 
-def int_func_for_A_x(t,x,y, B0):
-    if np.abs(t*x)<Lx/2 and np.abs(t*y)<Ly/2:
-        return -t*y*B0
-    else:
-        return 0
-def int_func_for_A_y(t,x,y,B0):
-    if np.abs(t*x)<Lx/2 and np.abs(t*y)<Ly/2:
-        return t*x*B0
-    else:
-        return 0
-
-def B_strength(x,y,lambd, threshold = 1, kappa=1, phi_0 = -np.pi):
-    if np.sqrt(x**2+y**2)<=Lx/4:
-        return -np.log(kappa)*phi_0/(2*np.pi*lambd**2)
-    else:
-        return -phi_0/(2*np.pi*lambd**2) * spspecial.kn(0, np.sqrt((x)**2+(y)**2)/lambd) * np.log(kappa)/spspecial.kn(0,threshold/lambd)
-
-
-def A_field_integrated_usadel(A, x, y, Nx, Ny, dx, dy, B0):
-    xv,yv = np.meshgrid(x,y)
-    for i in tqdm(range(xv.shape[0])):
-        for j in range(xv.shape[1]):
-            x_rel = xv[i,j]
-            y_rel = yv[i,j]
-            A[i,j,0] += spint.quad(int_func_for_A_x,0,1, args = (x_rel, y_rel,B0))[0]
-            A[i,j,1] += spint.quad(int_func_for_A_y,0,1, args = (x_rel, y_rel, B0))[0]
-                
-    return A
-
-
-
-Nx = 401
+Nx = 200
 Ny = Nx
 e = -1
-Lx = 12
-Ly = 12
+Lx = 4
+Ly = Lx+ Lx*0.0
 D = 1
-n = 7
+n = 1
 theta = n*2*np.pi
-eV = 0
+eV = 0.2
 tol = 1e-3
 gamma = 3
 use_kl = True
@@ -71,20 +42,19 @@ max_its = 1
 
 B0 = n*np.pi/((Lx+(0*dx))*(Ly+(0*dx)))
 B_and_bc = np.zeros((Ny+2, Nx+2,2))
-B_and_bc[0,:,0] = n*np.pi/(4*Lx)
-B_and_bc[-1,:,0] = -n*np.pi/(4*Lx)
-B_and_bc[:,0,1] = -n*np.pi/(4*Ly)
-B_and_bc[:,-1,1] = n*np.pi/(4*Ly)
+B_and_bc[0,1:-1,0] = n*np.pi/(4*Lx)
+B_and_bc[-1,1:-1,0] = -n*np.pi/(4*Lx)
+B_and_bc[1:-1,0,1] = -n*np.pi/(4*Ly)
+B_and_bc[1:-1,-1,1] = n*np.pi/(4*Ly)
 B_and_bc[1:-1,1:-1,1] = B0
 
 A = calculate_correct_A_field(B_and_bc, Nx, Ny, dx, dy)[1:-1,1:-1]
 slc = np.max((1, Nx//20))
-#A = np.zeros(A.shape, dtype = float)
-#A = A_field_integrated_usadel( A, x, y, Nx, Ny, dx, dy, B0)
 
-multiplier = 0
+
+
+multiplier = 1
 A*=multiplier
-
 B = np.gradient(A[:,:,0], dx, axis = 0) -  np.gradient(A[:,:,1], dy, axis = 1)
 
 print("Total phase in SC", theta)
@@ -105,8 +75,8 @@ epsilons = np.append(epsilons1,epsilons2)
 
 epsilons_minus = -epsilons
 
-epsilons+= -1j*(1e-2)#*(epsilons<=1)
-epsilons_minus+= -1j*(1e-2)#*(epsilons_minus>=-1)
+epsilons+= 1j*(1e-2)#*(epsilons<=1)
+epsilons_minus+= 1j*(1e-2)#*(epsilons_minus>=-1)
 
 A_delta = np.zeros_like(A)
 old_A_delta = np.ones_like(A)
@@ -137,7 +107,20 @@ while np.linalg.norm(A_delta-old_A_delta)>tol and num_its<max_its:
     print(np.max(A_delta), np.min(A_delta))
     print("Sum", np.sum(A_delta), "Average", np.average(A_delta), "Average abs", np.average(np.abs(A_delta)))
     print("Norm of difference from last iteration:", np.linalg.norm(A_delta-old_A_delta))
+    
 B_induced = np.gradient(A_delta[:,:,0], dx, axis = 0) -  np.gradient(A_delta[:,:,1], dy, axis = 1)
+B_app = np.gradient(A[:,:,0], dx, axis = 0) -  np.gradient(A[:,:,1], dy, axis = 1)
+fig, ax = plt.subplots(2,1, figsize = (20,15))
+ax[0].set_title("Applied magnetic field")
+a = ax[0].pcolormesh(x,y,B_app,cmap='seismic')
+fig.colorbar(a, ax = ax[0])
+ax[1].set_title("Induced magnetic field")
+a = ax[1].pcolormesh(x,y,B_induced,cmap='seismic')
+fig.colorbar(a, ax = ax[1])
+plt.suptitle(f'Magnetic fields for L={Lx}, N={Nx}, eV={eV}')
+#plt.savefig(f'Magnetic fields for surrounded normal metal with phase={2*n} pi L={Lx} N={Nx} eV={eV}.pdf')
+plt.close()
+
 plt.pcolormesh(x,y,B,cmap='seismic')
 plt.colorbar()
 plt.title("Applied magnetic field")
@@ -217,16 +200,41 @@ plt.show()
 
 
 plt.pcolormesh(x,y,abs_corr,cmap='seismic', norm=colors.LogNorm(vmin = np.sort(abs_corr.flatten())[1], vmax = np.max(np.abs(abs_corr))))
-plt.title("Absolute value of pair correlation")
+plt.title("n = " + str(n))
 plt.colorbar()
+if multiplier == 0:
+    plt.savefig(f'..\Plots\Pair corr plots\Amplitude plots\Amplitude of pair correlation with total phase {2*n} pi using N = {Nx}.pdf')
+else:
+    plt.savefig(f'..\Plots\Pair corr plots\Amplitude plots\Amplitude of pair correlation with A field and total phase {2*n} pi using N = {Nx}.pdf')
 plt.show()
 
 phases = np.arctan(pair_corr.imag/pair_corr.real)  
 a = int(np.round(theta/np.pi))
 plt.pcolormesh(x,y,phases,cmap='seismic',vmin = np.min(phases), vmax = np.max(phases))
-plt.title(f"Phase of pair correlation with total phase {a} $\pi$")
+plt.title("n = " + str(n))
 plt.colorbar()
-plt.savefig(f'.\Phase plots\Phase of pair correlation with total phase {a} pi using N = {Nx}.pdf')
+if multiplier == 0:
+    plt.savefig(f'..\Plots\Pair corr plots\Phase plots\Phase of pair correlation with total phase {2*n} pi using N = {Nx}.pdf')
+else:
+    plt.savefig(f'..\Plots\Pair corr plots\Phase plots\Phase of pair correlation with A field and total phase {2*n} pi using N = {Nx}.pdf')
+plt.show()
+
+plt.pcolormesh(x,y,phases, cmap = 'plasma',vmin = np.min(phases), vmax = np.max(phases))
+plt.title("n = " + str(n))
+plt.colorbar()
+plt.show()
+
+phases2 = np.arctan2(pair_corr.imag,pair_corr.real)  
+a = int(np.round(theta/np.pi))
+plt.pcolormesh(x,y,phases,cmap='cividis',vmin = np.min(phases), vmax = np.max(phases))
+plt.title("n = " + str(n))
+plt.colorbar()
+"""
+if multiplier == 0:
+    plt.savefig(f'..\Plots\Pair corr plots\Phase plots\Phase of pair correlation with arctan2 with total phase {a} pi using N = {Nx}.pdf')
+else:
+    plt.savefig(f'..\Plots\Pair corr plots\Phase plots\Phase of pair correlation with arctan2 with A field and total phase {a} pi using N = {Nx}.pdf')
+"""
 plt.show()
 # print profiling output
 #stats = pstats.Stats(prof).strip_dirs().sort_stats("cumtime")

2D Usadel integrated z-direction.py
@@ -270,10 +270,10 @@ def A_field_integrated_usadel(core_locs, A, x, y, Nx, Ny, dx, dy, threshold = 1,
 #prof.enable()
 
 #Set parameters for the run
-Nx = 101
+Nx = 100
 Ny = Nx
 e_ = -1
-Lx = 30
+Lx = 20
 Ly = Lx
 Lz = 1
 D = 1
@@ -286,30 +286,26 @@ include_gbcs = False
 kappa = 2.71
 nu = 0.8
 
-eV = 1/np.sqrt(2)
+eV = 0
 x,dx = np.linspace(-Lx/2,Lx/2,Nx,retstep = True)
 y, dy = np.linspace(-Ly/2,Ly/2,Ny,retstep = True)
 xv, yv = np.meshgrid(x,y)
 
+"""
+Lx2 = 70
+Ly2 = Lx2
 
-"""vortex_core_dist = 4.5
+vortex_core_dist = 12
 long_axis_dist = np.sin(np.pi/3) * vortex_core_dist
 short_axis_dist = np.sin(np.pi/6) * vortex_core_dist
 #vortex_core_dist = 1.075 * np.sqrt(np.abs(np.pi/B_applied))
+core_locs = []
 
-
-
-
-
-
-
-"""
-"""
-for i in range(int((Lx//2)//long_axis_dist)+1):
-    for j in range(int((Ly//2)//vortex_core_dist)+1):
+for i in range(int((Lx2//2)//long_axis_dist)+1):
+    for j in range(int((Ly2//2)//vortex_core_dist)+1):
         if i == 0 and j == 0:
             core_locs.append([0,0])
-        elif (j*vortex_core_dist + (i%2)*short_axis_dist + threshold)>Lx/2:
+        elif (j*vortex_core_dist + (i%2)*short_axis_dist + threshold)>Lx2/2:
             pass
         elif j==0 and i%2==1:
             core_locs.append([j*vortex_core_dist + (i%2)*short_axis_dist, i * long_axis_dist])
@@ -328,11 +324,16 @@ for i in range(int((Lx//2)//long_axis_dist)+1):
             core_locs.append([-(j*vortex_core_dist + (i%2)*short_axis_dist), i * long_axis_dist])
             core_locs.append([j*vortex_core_dist + (i%2)*short_axis_dist, -i * long_axis_dist])
             core_locs.append([-(j*vortex_core_dist + (i%2)*short_axis_dist), -i * long_axis_dist])
-print(core_locs)
+
 print(len(core_locs))
+
 core_locs = np.array(core_locs)
-"""
-"""
+print(np.where(core_locs[:,0]==np.max(core_locs[:,0])))
+
+core_locs = np.delete(core_locs, np.where(core_locs[:,0]==np.max(core_locs[:,0])),0)
+core_locs = np.delete(core_locs, np.where(core_locs[:,0]==np.max(core_locs[:,0])),0)
+core_locs[:,0] += vortex_core_dist/2
+
 core_locs = np.zeros((23,2))
 for i in range(6):
     core_locs[i,0] = np.cos(i*2*np.pi/6)*np.max(x)*5/12
@@ -344,7 +345,7 @@ for i in range(6):
 for i in range(4):
     core_locs[i+18,0] =  np.max(x)*10/12 * np.sign((i%2-1/2))
     core_locs[i+18,1] =  np.max(x)*10/12 * np.sin(1*np.pi/3) * np.sign(((i/3)%2-1/2))
-"""
+
 
 #Specify the center of the superconducting vortices 
 core_locs = np.zeros((7,2))
@@ -352,15 +353,13 @@ for i in range(6):
     core_locs[i,0] = np.cos(i*2*np.pi/6)*np.max(x)*5/6
     core_locs[i,1] = np.sin(i*2*np.pi/6)*np.max(y)*5/6
 
-#core_locs = np.zeros((1,2))
-
 plt.scatter(core_locs[:,0], core_locs[:,1])
-plt.xlim(np.min(x), np.max(x))
-plt.ylim(np.min(y), np.max(y))
+#plt.xlim(np.min(x), np.max(x))
+#plt.ylim(np.min(y), np.max(y))
 plt.grid()
 plt.show()
-
-#core_locs = np.zeros((1,2))
+"""
+core_locs = np.zeros((1,2))
 
 #Calculate the phase of the superconductor by adding the phase from each superconducting vortex
 theta = 0
@@ -382,6 +381,7 @@ A*=multiplier
 plt.plot(x, A[Ny//2,:,0], label = "Ax")
 plt.plot(x, A[Ny//2,:,1], label = "Ay")
 plt.grid()
+plt.legend()
 plt.show()
 
 
@@ -473,6 +473,10 @@ div_current_y = current_y[:-2]- current_y[2:]
 plt.quiver(x[slc//2::slc],y[slc//2::slc],current_x[slc//2::slc,slc//2::slc].real, current_y[slc//2::slc,slc//2::slc].real)
 plt.scatter(0,0)
 plt.title("Supercurrent")
+plt.show()
+plt.quiver(x[slc//2::slc],y[slc//2::slc],current_x_no_A[slc//2::slc,slc//2::slc].real, current_y_no_A[slc//2::slc,slc//2::slc].real)
+plt.scatter(0,0)
+plt.title("Supercurrent without A field")
 plt.show()
 
 plt.quiver(x[slc//2::slc],y[slc//2::slc],(current_x[slc//2::slc,slc//2::slc].real*0+1), (current_y[slc//2::slc,slc//2::slc].real*0+1), angles = (np.arctan2(current_y[slc//2::slc,slc//2::slc].real, current_x[slc//2::slc,slc//2::slc].real)*180.0/np.pi))
@@ -606,19 +610,7 @@ plt.colorbar()
 plt.title("Circulation of current")
 plt.show()
 
-N_from_center = 80
-plt.pcolormesh(xv[Ny//2-N_from_center:Ny//2+N_from_center,Nx//2-N_from_center:Nx//2+N_from_center],yv[Ny//2-N_from_center:Ny//2+N_from_center,Nx//2-N_from_center:Nx//2+N_from_center],circulating_current[Ny//2-N_from_center:Ny//2+N_from_center,Nx//2-N_from_center:Nx//2+N_from_center], cmap = 'seismic', vmin = -np.max(np.abs(circulating_current[Ny//2-N_from_center:Ny//2+N_from_center,Nx//2-N_from_center:Nx//2+N_from_center])), vmax = np.max(np.abs(circulating_current[Ny//2-N_from_center:Ny//2+N_from_center,Nx//2-N_from_center:Nx//2+N_from_center])))
-plt.colorbar()
-plt.title("Circulation of current")
-plt.show()
 
-
-
-
-plt.pcolormesh(xv[Ny//2-N_from_center:Ny//2+N_from_center,Nx//2-N_from_center:Nx//2+N_from_center],yv[Ny//2-N_from_center:Ny//2+N_from_center,Nx//2-N_from_center:Nx//2+N_from_center],circulating_current[Ny//2-N_from_center:Ny//2+N_from_center,Nx//2-N_from_center:Nx//2+N_from_center], cmap = 'seismic', vmin = -np.max(np.abs(circulating_current[Ny//2-N_from_center:Ny//2+N_from_center,Nx//2-N_from_center:Nx//2+N_from_center])), vmax = np.max(np.abs(circulating_current[Ny//2-N_from_center:Ny//2+N_from_center,Nx//2-N_from_center:Nx//2+N_from_center])))
-plt.colorbar()
-plt.title("Circulation of current")
-plt.show()
 
 plt.plot(x[Nx//2:],abs_corr[Ny//2, Nx//2:], label = "Pair corr")
 psi_0 = abs_corr[Ny//2, -1]

Boundary_conditions_for_linearized_Usadel.py
@@ -134,7 +134,8 @@ def f_integrated_z_direction(epsilon, theta, Nx, Ny, x, y, Lz=1, G_T=0.3, G_N=1,
         for i in range(core_locs.shape[0]):
             distance = np.sqrt((xv- core_locs[i,0])**2 + (yv - core_locs[i,1])**2)
             delta *= np.tanh(nu*(distance)) #*(distance>threshold)
-    f[1:-1, 1:-1] = (G_T/G_N) *(1/(Lz**2))*np.sinh(np.arctanh(delta/epsilon))*np.exp(1j*theta)
+    #Check if there should be a minus sign here
+    f[1:-1, 1:-1] = -(G_T/G_N) *(1/(Lz**2))*np.sinh(np.arctanh(delta/epsilon))*np.exp(1j*theta)
     f = np.reshape(f, (Nx+2)*(Ny+2))
     return f
 
@@ -227,6 +228,7 @@ def calculate_correct_A_field(B_and_bc, Nx, Ny, dx, dy):
     conditions into account. These equations are collected into a single matrix equation
     which is then solve. Except for the boundary conditions every other equation is 
     a divergence condition and every other equation is an equation for the B-field.
+    This does not work when Nx and Ny are odd and Nx+1 and Ny+1 are divisible by 4.
 
     Parameters
     ----------
@@ -255,10 +257,10 @@ def calculate_correct_A_field(B_and_bc, Nx, Ny, dx, dy):
     """
     #Create the diagonal to take into account boundary conditions
     diag = np.zeros_like(B_and_bc)
-    diag[:1] = 1
-    diag[-1:] = 1
-    diag[:,:1] = 1
-    diag[:,-1:] = 1
+    diag[0] = 1
+    diag[-1] = 1
+    diag[:,0] = 1
+    diag[:,-1] = 1
     
     #Create the upper and lower diagonals and fil them with values according to the finite difference scheme
     #This corresponds the the points with x+dx and x-dx when looking at a points (x,y)
@@ -285,15 +287,15 @@ def calculate_correct_A_field(B_and_bc, Nx, Ny, dx, dy):
     
     #If the number of points Nx and Ny are odd the middle point should be 0 by symmetry
     if Nx%2==1 and Ny%2==1:
-        right[Ny//2, Nx//2] = 0
-        left[Ny//2, Nx//2] = 0
-        over_x[Ny//2, Nx//2] = 0
-        over_y[Ny//2, Nx//2] = 0
-        under_x[Ny//2, Nx//2] = 0
-        under_y[Ny//2, Nx//2] = 0
-        diag[Nx//2, Ny//2] = 1
-        B_and_bc[Nx//2, Ny//2] = 0
-        
+        right[Ny//2+1, Nx//2+1] = 0
+        left[Ny//2+1, Nx//2+1] = 0
+        over_x[Ny//2+1, Nx//2+1] = 0
+        over_y[Ny//2+1, Nx//2+1] = 0
+        under_x[Ny//2+1, Nx//2+1] = 0
+        under_y[Ny//2+1, Nx//2+1] = 0
+        diag[Nx//2+1, Ny//2+1] = 1
+        B_and_bc[Nx//2+1, Ny//2+1] = 0
+      
     #Reshape all arrays to be one dimensional and fix them to be the proper length
     #The arrays to the rights ot the diagonal remove values from the end of the array while
     #arrays to the left remove from the start. This is since arrays to the right represent
@@ -312,7 +314,7 @@ def calculate_correct_A_field(B_and_bc, Nx, Ny, dx, dy):
     
     #Create a vector with the rhs of the equation
     g = np.reshape(B_and_bc, diag.shape[0])
-    
+
     #Solve the matrix equation to find the vector potential
     A = np.reshape(splinalg.spsolve(difference_matrix, g, use_umfpack = True), (Ny+2, Nx+2,2))
     return A

linearized_Usadel_solver_2D.py
@@ -56,7 +56,7 @@ def ghost_two_d_difference_matrix_usadel_neuman(x, y, dx, dy, A, eps, D=1, e_=-1
     diag = np.zeros((diag_y_shape,diag_x_shape), dtype = complex)       
     
     #All points excluding the ghost points follow normal finite difference scheme
-    diag[1:-1,1:-1] = -(2/(dx**2) + 2/(dy**2)  + 1j*2*(eps)/D -  4*e_*np.sum(A*A, axis = 2)) 
+    diag[1:-1,1:-1] = -(2/(dx**2) + 2/(dy**2)   +  4*e_**2*np.sum(A*A, axis = 2)) + 1j*2*(eps)/D
     #The ghost points at the end are different due to boundary conditions
     diag[0,:] = 1/(2*dy)
     diag[-1,:] = 1/(2*dy)
@@ -215,7 +215,7 @@ def get_Usadel_solution(epsilons_fun, f_sols_f,f_grads_f, bc_fun, x, y, dx, dy, 
     Ny = y.shape[0]
     #Loop through all energies to solve the linearized Usadel equation for
     #tqdm creates a progressbar to see how far in the simulation one has gotten
-    for i in range(epsilons_fun.shape[0]):
+    for i in tqdm(range(epsilons_fun.shape[0])):
         #Create the matrix describing the finite difference version of the linearized Usadel
         #equation where the lhs contains all coefficents in front of the various f_ij
         #using ghost points so the boundary conditions are also included
@@ -274,14 +274,14 @@ def update_A_field(x_grid, y_grid, x_current, y_current, dx, dy, mu_0 = 1):
             distances = np.sqrt((x_grid[i,j]-x_grid)**2+(y_grid[i,j]-y_grid)**2)
             #Set the smallest distance which should be zero equal to the second smallest 
             #distance. This is done to avoid infinities when dividing by the distance
-            distances[distances==0.0] = np.min(distances + (distances==0)*np.max(distances))
+            distances[distances==0.0] = np.inf #np.min(distances + (distances==0)*np.max(distances))
             #Calculate the inverse of the distance
             inverse_distances = 1/distances
             #Calculate the contribution to the A-field in the x and y direction using 
             #Maxwells equations. The multiplication with dx and dy is to take into account
             #that we are integrating
-            delta_A[i,j,0] = mu_0/(4*np.pi) * np.trapz(np.trapz(x_current/inverse_distances, x=x_grid[0], axis = 1), x = y_grid[:,0])
-            delta_A[i,j,1] = mu_0/(4*np.pi) * np.trapz(np.trapz(y_current/inverse_distances, x=x_grid[0], axis = 1), x = y_grid[:,0])
+            delta_A[i,j,0] = mu_0/(4*np.pi) * np.trapz(np.trapz(x_current*inverse_distances, x=x_grid[0], axis = 1), x = y_grid[:,0])
+            delta_A[i,j,1] = mu_0/(4*np.pi) * np.trapz(np.trapz(y_current*inverse_distances, x=x_grid[0], axis = 1), x = y_grid[:,0])
             
             """delta_A[i,j,0] = mu_0/(4*np.pi) * np.sum(x_current/inverse_distances)*dx*dy
             delta_A[i,j,1] = mu_0/(4*np.pi) * np.sum(y_current/inverse_distances)*dx*dy"""
@@ -435,7 +435,7 @@ def ghost_two_d_integrated_difference_matrix_usadel(x, y, dx, dy, A, eps, D=1, e
     #Create the diagonal of the matrix and fill it with the proper values
     diag = np.zeros((diag_y_shape,diag_x_shape), dtype = complex)       
     #All points excluding the ghost points follow normal finite difference scheme
-    diag[1:-1,1:-1] = -(2/(dx**2) + 2/(dy**2)  + 1j*2*(eps)/D -  4*e_*np.sum(A*A, axis = 2)) 
+    diag[1:-1,1:-1] = -(2/(dx**2) + 2/(dy**2)   +  4*e_**2*np.sum(A*A, axis = 2)) + 1j*2*(eps)/D
 
     if core_locs[0,0]!=None and include_gbcs:
             delta = 1